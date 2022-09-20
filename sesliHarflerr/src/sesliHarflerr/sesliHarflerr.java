6e941591ae3a5f56533ce0996b5c4ea4455a0d76
duhann17 <duhan.uyan17@gmail.com>
2022-09-21 01:03:55 +0300

    first week hw

---
+++ sesliHarflerr/src/sesliHarflerr/sesliHarflerr.java
@@ -0,0 +1,24 @@
+package sesliHarflerr;
+
+public class sesliHarflerr {
+
+	public static void main(String[] args) {
+		char harf='C';
+		
+		switch(harf) { 
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+		System.out.println("Kalın sesli harf");
+		break;
+		default:
+		System.out.println("İnce sesli harf");
+	
+			
+		
+		
+		}
+ 	}
+
+}
